5fe9d60e1816a2fdf59aaf0cf869bda92390d5d5
Heitor Santos <heitorg33@gmail.com>
2025-03-17 08:41:09 -0300

    include new rote

--- src/router/route.js
+++ src/router/route.js
@@ -5,9 +5,13 @@ const {
 const {
   integrationClientsController,
 } = require("../controller/integrationClientsController");
+const {
+  associationController,
+} = require("../controller/associationController");
 const router = Router();
 
 router.post("/create-products", integrationProductsController);
 router.get("/get-clients", integrationClientsController);
+router.post("include-fields", associationController);
 
 module.exports = { router };
