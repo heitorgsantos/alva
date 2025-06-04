c93233c8a6e31bd16c17651f5a9a9e2f2fa9ff27
Heitor Santos <heitorg33@gmail.com>
2025-06-04 07:44:05 -0300

    fix redis

--- src/queues/webhookQueue.js
+++ src/queues/webhookQueue.js
@@ -9,7 +9,6 @@ const getRedisConnectionConfig = () => {
   return {
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: parseInt(process.env.REDIS_PORT || "6379", 10),
-    password: process.env.REDIS_PASSWORD,
   };
 };
 
