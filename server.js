2dc45ff5c7db15cdf5305bc326b1a5a9f37668f2
Heitor Santos <heitorg33@gmail.com>
2025-06-06 08:00:37 -0300

    fix redis

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require("./src/workers/webhookWorker"); // This will start the worker
 const express = require("express");
 const cors = require("cors");
 const { router } = require("./src/router/route"); // Ensure this path is correct
 require("dotenv").config();
-require("./src/workers/webhookWorker"); // This will start the worker
 
 const app = express();
 const port = process.env.PORT || 3000;
