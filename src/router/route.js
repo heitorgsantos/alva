0b98a2fdf932cb7a864b6fdd9d1199cde2b8d4be
Heitor Santos <heitorg33@gmail.com>
2025-06-11 15:11:44 -0300

    correção da rota

--- src/router/route.js
+++ src/router/route.js
@@ -5,6 +5,6 @@ const {
 
 const router = Router();
 
-router.post("/create-files/sandbox", webHookCreateFieldsController);
+router.post("/create-files-sandbox", webHookCreateFieldsController);
 
 module.exports = { router };
